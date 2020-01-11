f33131b4e53676450915c5230c803b6d5729ee4c
Lena L <lena.lysenko.work@gmail.com>
2020-01-11 13:55:23 +0100

    #11 Add pictures from both camera and photo library, enable editing

--- CatsML/CatsML/ViewController.swift
+++ CatsML/CatsML/ViewController.swift
@@ -60,12 +60,38 @@ class ViewController: UIViewController {
     }
 
     @IBAction func selectButtonPressed(_ sender: Any) {
+        let alertController  = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-            let myPickerController = UIImagePickerController()
-            myPickerController.delegate = self
-            myPickerController.sourceType = .photoLibrary
-            present(myPickerController, animated: true, completion: nil)
+            alertController.addAction(UIAlertAction(title: "Photo library", style: .default, handler: { _ in
+                self.openPhotoLibrary()
+            }))
         }
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            alertController.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+                self.openCamera()
+            }))
+        }
+
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        present(alertController, animated: true, completion: nil)
+    }
+
+    private func openPhotoLibrary() {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self
+        myPickerController.sourceType = .photoLibrary
+        present(myPickerController, animated: true, completion: nil)
+    }
+
+    private func openCamera() {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self
+        myPickerController.sourceType = .camera
+        myPickerController.allowsEditing = true
+        present(myPickerController, animated: true, completion: nil)
     }
 
     @IBAction func moreButtonPressed(_ sender: Any) {
@@ -90,12 +116,13 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[.originalImage] as? UIImage {
+        var image: UIImage? = info[.editedImage] as? UIImage
+        if image == nil { image = info[.originalImage] as? UIImage }
+
+        if image != nil {
             imageView.image = image
             backgroundImageView.image = image
             scanImage()
-        } else{
-            print("Something went wrong")
         }
 
         dismiss(animated: true, completion: nil)
